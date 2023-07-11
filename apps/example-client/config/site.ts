15337c2ec58c2a6ad6fa6fc3ffe13b8fcfcabf24
Luis Taniça <matallui@gmail.com>
2023-07-11 08:47:39 -0600

    example-client: fix title

--- apps/example-client/config/site.ts
+++ apps/example-client/config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Artico",
+  description: "WebRTC made simple",
   mainNav: [
     {
       title: "Home",
