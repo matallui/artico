50b2397296b0c4a528ba3477e7f2abeb67e21567
Luis Taniça <matallui@gmail.com>
2024-02-09 21:51:32 -0700

    chore(example-peer): add index.html to tailwind config

--- examples/peer/tailwind.config.ts
+++ examples/peer/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
+    './index.html',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
