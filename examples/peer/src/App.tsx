4bbc494ffd727c283fb442aa222c65ab8f524259
Luis Taniça <matallui@gmail.com>
2024-02-09 21:54:23 -0700

    fix(example-peer): fix mobile

--- examples/peer/src/App.tsx
+++ examples/peer/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
     <div className="min-h-[100vh] bg-background text-foreground dark flex flex-col items-center gap-4 py-20">
       <h1 className="text-4xl">Peer Example</h1>
       <div className="flex flex-col lg:flex-row gap-4 p-4">
-        <PeerConsole className="w-[490px]" name="A" initiator />
-        <PeerConsole className="w-[490px]" name="B" />
+        <PeerConsole className="flex-1 sm:w-[490px]" name="A" initiator />
+        <PeerConsole className="flex-1 sm:w-[490px]" name="B" />
       </div>
     </div>
   );
