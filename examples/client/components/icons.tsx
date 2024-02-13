668a088bfdba5f12b59e573698f39e2ee54e3ef7
Luis Taniça <matallui@gmail.com>
2024-02-13 08:35:45 -0700

    fix(example-client): fix buid

--- examples/client/components/icons.tsx
+++ examples/client/components/icons.tsx
@@ -2,16 +2,11 @@ import {
   LucideProps,
   Moon,
   SunMedium,
-  Twitter,
-  type Icon as LucideIcon,
 } from "lucide-react"
-
-export type Icon = LucideIcon
 
 export const Icons = {
   sun: SunMedium,
   moon: Moon,
-  twitter: Twitter,
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
