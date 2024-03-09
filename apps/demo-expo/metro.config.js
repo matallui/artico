c9f3db2ddf05f95a3d8d64743687e7494e006ae3
Luis Taniça <matallui@gmail.com>
2024-03-09 12:32:52 -0700

    chore(demo-expo): setup react-native-webrtc

--- apps/demo-expo/metro.config.js
+++ apps/demo-expo/metro.config.js
@@ -4,11 +4,14 @@ const { withNativeWind } = require("nativewind/metro");
 const path = require("path");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = withMonorepoPaths(
-  withNativeWind(config, { input: "./styles/global.css" })
+const config = withMonorepoPaths(
+  withEventTargetShim(
+    withNativeWind(defaultConfig, { input: "./styles/global.css" }),
+  ),
 );
+module.exports = config;
 
 /**
  * Add the monorepo paths to the Metro config.
@@ -31,5 +34,36 @@ function withMonorepoPaths(config) {
     path.resolve(workspaceRoot, "node_modules"),
   ];
 
+  return config;
+}
+
+/**
+ * Need this for react-native-webrtc to work with Expo SDK 50
+ * @see https://github.com/expo/config-plugins/tree/main/packages/react-native-webrtc#event-target-shim
+ * @param {import('expo/metro-config').MetroConfig} config
+ * @returns {import('expo/metro-config').MetroConfig}
+ */
+function withEventTargetShim(config) {
+  config.resolver.resolveRequest = (context, moduleName, platform) => {
+    if (
+      // If the bundle is resolving "event-target-shim" from a module that is part of "react-native-webrtc".
+      moduleName.startsWith("event-target-shim") &&
+      context.originModulePath.includes("react-native-webrtc")
+    ) {
+      // Resolve event-target-shim relative to the react-native-webrtc package to use v6.
+      // React Native requires v5 which is not compatible with react-native-webrtc.
+      const eventTargetShimPath = resolveFrom(
+        context.originModulePath,
+        moduleName,
+      );
+
+      return {
+        filePath: eventTargetShimPath,
+        type: "sourceFile",
+      };
+    }
+    // Ensure you call the default resolver.
+    return context.resolveRequest(context, moduleName, platform);
+  };
   return config;
 }
