72ffd0b13f517bc9addf52865b19f6f92f3c7065
Luis Taniça <matallui@gmail.com>
2024-08-24 12:26:58 -0600

    fix(demo): demo links

--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "~/components/ui/button";
+import { buttonVariants } from "~/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -7,15 +7,24 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24 gap-8 relative">
       <Image src="/logo.png" alt="Artico logo" width={100} height={100} />
       <h1 className="text-4xl font-bold">Artico Demos</h1>
-      <Button variant="secondary" size="lg">
-        <Link href="/peer">Peer Demo</Link>
-      </Button>
-      <Button variant="secondary" size="lg">
-        <Link href="/call">Call Demo</Link>
-      </Button>
-      <Button variant="secondary" size="lg">
-        <Link href="/room">Room Demo</Link>
-      </Button>
+      <Link
+        href="/peer"
+        className={buttonVariants({ variant: "secondary", size: "lg" })}
+      >
+        Peer Demo
+      </Link>
+      <Link
+        href="/call"
+        className={buttonVariants({ variant: "secondary", size: "lg" })}
+      >
+        Call Demo
+      </Link>
+      <Link
+        href="/room"
+        className={buttonVariants({ variant: "secondary", size: "lg" })}
+      >
+        Room Demo
+      </Link>
     </main>
   );
 }
