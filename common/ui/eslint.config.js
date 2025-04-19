4f53341bbaa64e86a305e8507b6478a733ea9c4a
Luis Taniça <matallui@gmail.com>
2025-04-19 14:49:40 -0600

    drop tooling/tailwind, add common/ui

---
+++ common/ui/eslint.config.js
@@ -0,0 +1,11 @@
+import baseConfig from "@rtco/eslint-config/base";
+import reactConfig from "@rtco/eslint-config/react";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ["dist/**"],
+  },
+  ...baseConfig,
+  ...reactConfig,
+];
